80513c26cea926bbab32f4b5052a02d788fe56e8
manu.kondapi@fattmerchant.com <manu.kondapi@fattmerchant.com>
2023-02-03 19:05:47 +0000

    edits to grant_select macro

--- macros/grant_select.sql
+++ macros/grant_select.sql
@@ -1,10 +1,10 @@
 
 {% macro grant_select(user) %}
 {% set sql %}
-    grant usage on schema {{ target.schema }} to {{ user }};
-    grant select on all tables in schema {{ target.schema }} to {{ user }};
-    grant select on all views in schema {{ target.schema }} to {{ user }};
-    ALTER DEFAULT PRIVILEGES IN SCHEMA {{ target.schema }} GRANT SELECT ON TABLES TO {{ user }};
+    grant usage on schema {{ target.schema }} to "{{ user }}";
+    grant select on all tables in schema {{ target.schema }} to "{{ user }}";
+    --grant select on all views in schema {{ target.schema }} to {{ user }};
+    ALTER DEFAULT PRIVILEGES IN SCHEMA {{ target.schema }} GRANT SELECT ON TABLES TO "{{ user }}";
 
 {% endset %}
 
