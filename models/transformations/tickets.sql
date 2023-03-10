298c8c171bde4f232b63aabade4b15fbccc1e13d
manu.kondapi@fattmerchant.com <manu.kondapi@fattmerchant.com>
2023-03-10 14:08:02 +0000

    tickets update

--- models/transformations/tickets.sql
+++ models/transformations/tickets.sql
@@ -9,7 +9,6 @@ t.created_date,
 t.ticket_closed_date,
 t.deal_closed_date,
 t.pipeline, 
-tph.timestamp_instant as last_touched_by
 t.ticket_category,
 t.ticket_owner_id,
 t.ticket_owner,
@@ -23,8 +22,3 @@ t.uw_ticket_category,
 t.uw_high_ticket
 from {{ ref('hubspot_tickets') }} as t
 left join {{ ref('hubspot_ticket_property_history')}} tph on t.ticket_id = tph.ticket_id and t.ticket_owner_id = tph.value
-where tph.most_recent = 1
--- left outer join
---     {{ ref('deal_pipelines__stages') }} as s on d.property_dealstage__value = s.stageid
--- left outer join  {{ ref('owners') }} as o on d.property_hubspot_owner_id__value = o.ownerid
--- left outer join {{ ref('deal_pipelines') }} as dp on d.property_pipeline__value = dp.pipelineid
