d0e5f540e0a859a23353fd3908d8238b66c24258
manu.kondapi@fattmerchant.com <manu.kondapi@fattmerchant.com>
2023-03-13 20:34:29 +0000

    tickets table adds

--- models/transformations/ticket_review_time.sql
+++ models/transformations/ticket_review_time.sql
@@ -1,16 +1,17 @@
 --  Remove time in Increase Requests and Account Update numbers to get a true "investigation time"
+-- of the tickets closed this month: how long did it take to resolve?
 
-select week, 
-       --sum(case when ticket_closed_date is not null then 1 else 0 end) as num_closed_tickets,
-       --sum(ticket_timetoclose) as timetoclose,
-       round(cast(sum(ticket_timetoclose) as numeric(10,2))/sum(case when ticket_closed_date is not null then 1 else 0 end), 2) as avg_time_taken
-from (
+select timeline, 
+      avg(cast(ticket_timetoclose as numeric(10,2))),
+      count(*)
+    from (
 select 
-date_trunc('week', created_date) as week,
+date_trunc('week', ticket_closed_date) as timeline,
 case when waiting_on_merchant_time is not null then time_to_close - waiting_on_merchant_time
     else time_to_close end as ticket_timetoclose, *
 from {{ ref('tickets') }} as t
-where created_date >= '2022-10-01'
+where ticket_closed_date >= '2022-10-01'
+and ticket_owner not like 'Other Analyst'
 )
-group by week
-order by week desc
+group by timeline
+order by timeline desc
